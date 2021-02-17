424fe9dcd558aee2cdd719a83927fa8a3932336c
oasis02k <79162981+oasis02k@users.noreply.github.com>
2021-02-17 14:26:51 -0500

    Add files via upload

---
+++ Task 1/js/myscript.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('.form-container form').on('submit', function(event){ 
+
+        event.preventDefault(); // prevent the form from actually changing the browser URL
+
+        $('.button-text').addClass('button-loading'); // make the spinner visible
+        $('.button-text').text('');
+
+        setTimeout(function(){
+            $('.popup-container').removeClass('hide');
+            $('.form-container').addClass('hide');
+        }, 3000); // after 3 seconds, hide the form and show the popup
+
+    });
+
+    $('.dismiss-button').on('click', function(){
+	    
+
+        $('.popup-container').addClass('hide');// Make the popup container disappear
+
+    });
+});
